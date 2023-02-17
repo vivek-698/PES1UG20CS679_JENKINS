e89ede9e38415b646772bc7b53ac91e70176c7fc
vivek-698 <85101944+vivek-698@users.noreply.github.com>
2023-02-17 20:04:23 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully changed the file and it is running automatically\n";
+  cout << "This is the Chnaged file\n";
   return 0;
 }
